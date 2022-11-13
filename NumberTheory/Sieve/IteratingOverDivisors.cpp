35299492ce9a2442adb40acdae2ae4b7387dd127
navjotdadwal <59385365+navjotdadwal@users.noreply.github.com>
2022-11-13 15:59:45 +0530

    Create IteratingOverDivisors.cpp

---
+++ NumberTheory/Sieve/IteratingOverDivisors.cpp
@@ -0,0 +1,13 @@
+// int divisors[n + 1];
+// for (int i = 1; i <= n; ++i)
+//  for (int j = i; j <= n; j += i)
+//   ++divisors[j];
+
+void precompute()
+{
+  for (int i = 2; i < N; i++) {
+    for (int j = i + i; j < N; j += i){
+        
+    }
+  }  
+}
